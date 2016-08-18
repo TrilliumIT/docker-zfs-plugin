1a65ee84e7cf1b5af8a4c75c37f0055e15784c0a
Travis Hegner <thegner@trilliumit.com>
2016-08-18 17:01:23 -0400

    initial main

--- main.go
+++ main.go
@@ -2,10 +2,58 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/TrilliumIT/docker-zfs-plugin/zfs"
 	"github.com/docker/go-plugins-helpers/volume"
 	"github.com/urfave/cli"
 )
 
+const (
+	version = 0.1
+)
+
 func main() {
+
+	var flagBaseDir = cli.StringFlag{
+		Name:  "base-dir",
+		Value: "/var/lib/docker-volumes",
+		Usage: "Base directory where the driver root will be created.",
+	}
+
+	var flagRootDir = cli.StringFlag{
+		Name:  "root-dir",
+		Value: "zfs",
+		Usage: "Relative name of the root directory for the driver. All volumes will be created in $BaseDir/$RootDir.",
+	}
+
+	app := cli.NewApp()
+	app.Name = "docker-zfs-plugin"
+	app.Usage = "Docker ZFS Plugin"
+	app.Version = version
+	app.Flags = []cli.Flag{
+		flagBaseDir,
+		flagRootDir,
+	}
+	app.Action = Run
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Run(ctx *cli.Context) {
+	_, err := FileInfo.Stat(ctx.String("base-dir"))
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Error("Base directory must exist when launching docker-zfs-plugin.")
+			return err
+		}
+	}
+	d, err := vxlan.NewDriver(ctx.String("base-dir") + os.PathSeparator + ctx.String("root-dir"))
+	if err != nil {
+		panic(err)
+	}
+	h := volume.NewHandler(d)
+	h.ServeUnix("root", "zfs")
 }
--- zfs/driver.go
+++ zfs/driver.go
@@ -2,9 +2,16 @@ package zfs
 
 import (
 	"fmt"
+
 	"github.com/docker/go-plugins-helpers/volume"
 )
 
 type Driver struct {
 	volume.Driver
+}
+
+func NewDriver(dspath string) (Driver, error) {
+	fmt.Println("test")
+
+	return Driver{}, nil
 }
